4a6fcb93e44f88cb515a9978cfc4b28b943033be
Peter Chu <cgpchu@uwaterloo.ca>
2016-01-16 16:39:13 -0500

    check for already visited images

--- nofap.js
+++ nofap.js
@@ -3,7 +3,7 @@ function Cat(ratio, imageurl){
   this.imageurl = imageurl;
 }
 
- var getCat = {
+var getCat = {
   init: function(myCat){
     this.myCat = myCat;
   },
@@ -25,17 +25,19 @@ function Cat(ratio, imageurl){
       return myCat.ratio === "square";
     });  
   }
- };
+};
 
- function Randomize(images){
-    return Math.floor(Math.random() * images.length)
- }
+function Randomize(images){
+  return Math.floor(Math.random() * images.length)
+}
 
 var myCat = [ 
-new Cat("horizontal", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
-new Cat("vertical", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
-new Cat("square", "http://www.helpinghomelesscats.com/images/cat1.jpg")
+  new Cat("horizontal", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
+  new Cat("vertical", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
+  new Cat("square", "http://www.helpinghomelesscats.com/images/cat1.jpg")
 ]
+
+var visitedImages = {}
 
 function imageRatio(image) {
   var proportion = image.height/image.width;
@@ -48,17 +50,20 @@ function imageRatio(image) {
     return "horizontal";
   }
 }     
-           
+
 (function (document) {
-  
+
   getCat.init(myCat);
-  var images = document.getElementsByTagName('img'), length = images.length
- 
+  var images = document.getElementsByTagName('img'), length = images.length;
+
   for (var i = 0; i < length; i++) {
-    var ratio = imageRatio(images[i]);
-    var number = Randomize(getCat[ratio]());
-    var img = getCat[ratio]()[number];
-    images[i].src = img.imageurl
+    if(!(img.imageurl in visitedImages)) {
+      var ratio = imageRatio(images[i]);
+      var number = Randomize(getCat[ratio]());
+      var img = getCat[ratio]()[number];
+      images[i].src = img.imageurl;
+      visitedImages[img.imageurl] = true; // move this to response callback from api request
+    }
   }
 
 })(document);
