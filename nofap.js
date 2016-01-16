a571d79785c9b4107eda33248567c1fdbaa5ef53
Jake Si <jake.si@live.com>
2016-01-16 17:15:55 -0500

    ajax request

--- nofap.js
+++ nofap.js
@@ -37,6 +37,29 @@ new Cat("vertical", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
 new Cat("square", "http://www.helpinghomelesscats.com/images/cat1.jpg")
 ]
 
+function filterImage(imgUrl){
+  console.log(imgUrl);
+  $.ajax({ 
+      headers : {
+        "authorization": "Basic YWNjX2M1OWNlNDY5OTAxNDNiYzpkZDM2YzQzM2JjODliODc0OGUwNzJhZGZiZjFmNTNkYQ==",
+        "accept": "application/json"
+      },
+      type: "GET",
+      dataType: "json",
+      url: "https://api.imagga.com/v1/tagging",
+      data: {
+        "url": imgUrl
+      },
+      success: function(data){        
+        alert(data);
+      },
+      error: function(data){
+        alert("ASDGSDFGSDFG");
+      }
+
+    });
+}
+
 function imageRatio(image) {
   var proportion = image.height/image.width;
 
@@ -52,12 +75,16 @@ function imageRatio(image) {
 (function (document) {
   
   getCat.init(myCat);
-  var images = document.getElementsByTagName('img'), length = images.length
- 
+
+  var images = document.getElementsByTagName('img')
+  var length = images.length
+  
+  filterImage(images[0]);
+
   for (var i = 0; i < length; i++) {
     var ratio = imageRatio(images[i]);
-    var number = Randomize(getCat[ratio]());
-    var img = getCat[ratio]()[number];
+    // var number = Randomize(getCat[ratio]());
+    var img = getCat["horizontal"]()[0];
     images[i].src = img.imageurl
   }
 
