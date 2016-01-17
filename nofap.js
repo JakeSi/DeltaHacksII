374c037a9e24eb3b09f1bca6c8f8aac9a29b3338
shashanksabhlok <shashanksabhlok95@gmail.com>
2016-01-17 02:50:39 -0400

    wip

---
+++ nofap.js
@@ -0,0 +1,173 @@
+function Cat(ratio, imageurl){
+  this.ratio = ratio;
+  this.imageurl = imageurl;
+}
+var backgroundURL ="";
+var token = "";
+
+var getCat = {
+  init: function(myCat){
+    this.myCat = myCat;
+  },
+
+  horizontal: function(){
+    return this.myCat.filter(function(myCat){
+      return myCat.ratio === "horizontal";
+    });
+  },
+
+  vertical: function(){
+    return this.myCat.filter(function(myCat){
+      return myCat.ratio === "vertical";
+    });
+  }, 
+
+  square: function (){
+    return this.myCat.filter(function(myCat){
+      return myCat.ratio === "square";
+    });  
+  }
+};
+
+function Randomize(images){
+  return Math.floor(Math.random() * images.length)
+}
+
+var myCat = [ 
+  new Cat("horizontal", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
+  new Cat("vertical", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
+  new Cat("square", "http://www.helpinghomelesscats.com/images/cat1.jpg")
+]
+
+var visitedImages = {
+  "http://www.helpinghomelesscats.com/images/cat1.jpg" : false
+}
+
+var nsfwTags = {
+  "sexy":true,
+  "attractive":true,
+  "erotic":true,
+  "model":true,
+  "porn":true,
+  "boob":true,
+  "breast":true,
+  "brassiere":true,
+  "bra":true,
+  "butt":true,
+  "cleavage":true,
+  "penis":true,
+  "dick":true,
+  "naked":true,
+  "underwear":true,
+  "lingerie":true,
+  "pantie":true
+}
+
+function isNSFW(tags){
+  var numTags = Math.min(tags.length,10);
+  for (var i = 0; i < numTags; i++) {
+    if (tags[i] in nsfwTags) {
+      return true;
+    };
+  };
+
+  return false;
+}
+
+function filterImage(img){
+  var authbearer = " Bearer " + token;
+  $.ajax({ 
+      headers : {
+        "Authorization": authbearer
+      },
+      type: "POST",
+      url: "https://api.clarifai.com/v1/tag/",
+      data: {
+        "url": img.src
+      },
+      success: function(data){
+      console.log(data);       
+        if(isNSFW(data.results[0].result.tag.classes)){
+          console.log("NSFW");
+          console.log(img.src);
+           visitedImages[img.src] = true;
+          img.src = getCat["horizontal"]()[0].imageurl;
+        } else {
+           visitedImages[img.src] = false;
+        }
+      },
+      error: function(data){
+        console.log(data);
+      }
+
+  });
+}
+
+function imageRatio(image) {
+  var proportion = image.height/image.width;
+
+  if(proportion > 1) {
+    return "vertical";
+  } else if(proportion === 1) {
+    return "square";
+  } else if(proportion < 1) {
+    return "horizontal";
+  }
+}     
+
+function fetchApiToken(image){
+  $.ajax({ 
+
+    type: "POST",
+    dataType: "json",
+    url: "https://api.clarifai.com/v1/token/",
+    data: {
+      "client_id" : "AmzUl2hfw42BUya2_0Z66BWwPjmQog81P9etLF8x",
+      "client_secret"  :"FQfN5rjUhlXkwtiLdN_Gxz998MVEVG2KiL1yceD8",
+      "grant_type" : "client_credentials"
+    },
+    async: false,
+    success: function(data){
+      console.log(data);
+      token = data.access_token;       
+    },
+    error: function(data){
+      console.log(data);
+    }
+
+  });
+}
+function background(){
+    $("div").each(function(i, img) {
+     backgroundURL = (this).css("background-image").url;
+        });
+	 alert(backgroundURL);
+    }  
+
+(function (document) {
+
+  test = function(){
+    var images = document.getElementsByTagName('img')
+    var length = images.length
+
+    for (var i = 0; i < length; i++) {
+      var ratio = imageRatio(images[i]);
+
+      // is cached as nsfw or not in cache yet
+      if(!(images[i].src in visitedImages)) {
+        if (images[i].height > 75 && images[i].width > 75) {
+          filterImage(images[i]);
+        };
+      }else if(visitedImages[images[i].src]){
+        image[i].src = getCat["horizontal"]()[0].imageurl;
+      }
+      // var number = Randomize(getCat[ratio]());
+    }
+  }
+
+  getCat.init(myCat);
+
+  fetchApiToken();
+  test();
+  setInterval(test,1000);
+})(document);
