81003a269d372237d115f4fa899502d617eb63e1
Jake Si <jake.si@live.com>
2016-01-17 01:30:57 -0500

    added more cat pics and changed ratio algorithm

--- nofap.js
+++ nofap.js
@@ -36,25 +36,50 @@ function Randomize(images){
 var myCat = [ 
   new Cat("horizontal", "https://indiabright.com/wp-content/uploads/2015/12/adorable-angelic-animal-baby-cat-cute-Favim.com-44596.jpg"),
   new Cat("horizontal", "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg"),
-  new Cat("horizontal", "https://nextranks.com/data_images/main/cats/cats-04.jpg"),
-  new Cat("vertical", "https://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg"),
-  new Cat("vertical", "https://www.artistiqueacres.com/images/cute-cat.jpg"),
+  new Cat("horizontal", "http://nextranks.com/data_images/main/cats/cats-04.jpg"),
+  new Cat("horizontal", "http://images.natureworldnews.com/data/images/full/19781/picky-cat.jpg"),
+  new Cat("horizontal", "https://i.imgur.com/UQmua1N.jpg"),
+  new Cat("horizontal", "https://i.imgur.com/7SJajco.jpg"),
+  new Cat("horizontal", "https://i.imgur.com/fMb19BZ.jpg"),
+  new Cat("vertical", "https://i.imgur.com/7QU8HaP.jpg"),
+  new Cat("vertical", "https://i.imgur.com/fRxkJV2.jpg"),
   new Cat("vertical", "https://s-media-cache-ak0.pinimg.com/236x/40/c1/bd/40c1bd174fa0f13f6a025e6e7e44bec4.jpg"),
+  new Cat("vertical", "http://www.cat-breeds-encyclopedia.com/image-files/siamese-cat-portrait.jpg"),
+  new Cat("vertical", "https://i.imgur.com/asPsIEH.jpg"),
+  new Cat("vertical", "https://i.imgur.com/x80K9qC.jpg"),
+  new Cat("vertical", "https://i.imgur.com/ngTmiFN.jpg"),
   new Cat("square", "https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg"),
-  new Cat("square", "https://www.helpinghomelesscats.com/images/cat1.jpg"),
-  new Cat("square", "https://s.hswstatic.com/gif/abyssinian-cat.jpg"),
-  new Cat("square", "https://i.imgur.com/zLDuTkV.jpg")
+  new Cat("square", "http://www.helpinghomelesscats.com/images/cat1.jpg"),
+  new Cat("square", "http://s.hswstatic.com/gif/abyssinian-cat.jpg"),
+  new Cat("square", "https://i.imgur.com/zLDuTkV.jpg"),
+  new Cat("square", "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg"),
+   new Cat("square", "https://pbs.twimg.com/profile_images/469217937862037504/yd3-lohT.jpeg")
+
 ]
+
 var visitedImages = {
   "https://indiabright.com/wp-content/uploads/2015/12/adorable-angelic-animal-baby-cat-cute-Favim.com-44596.jpg" : false,
   "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg" : false,
-  "https://nextranks.com/data_images/main/cats/cats-04.jpg" : false,
-  "https://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg": false,
-  "https://www.artistiqueacres.com/images/cute-cat.jpg": false,
+  "http://nextranks.com/data_images/main/cats/cats-04.jpg" : false,
+  "http://images.natureworldnews.com/data/images/full/19781/picky-cat.jpg":false,
+  "https://i.imgur.com/UQmua1N.jpg":false,
+  "https://i.imgur.com/7SJajco.jpg":false,
+  "https://i.imgur.com/fMb19BZ.jpg":false,
+
+  "https://i.imgur.com/7QU8HaP.jpg": false,
+  "https://i.imgur.com/fRxkJV2.jpg": false,
   "https://s-media-cache-ak0.pinimg.com/236x/40/c1/bd/40c1bd174fa0f13f6a025e6e7e44bec4.jpg": false,
+  "http://www.cat-breeds-encyclopedia.com/image-files/siamese-cat-portrait.jpg":false,
+  "https://i.imgur.com/asPsIEH.jpg":false,
+  "https://i.imgur.com/x80K9qC.jpg":false,
+  "https://i.imgur.com/ngTmiFN.jpg":false,
+
   "https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg": false,
-  "https://www.helpinghomelesscats.com/images/cat1.jpg" : false,
-  "https://s.hswstatic.com/gif/abyssinian-cat.jpg": false
+  "http://www.helpinghomelesscats.com/images/cat1.jpg" : false,
+  "https://s.hswstatic.com/gif/abyssinian-cat.jpg": false,
+  "https://i.imgur.com/zLDuTkV.jpg": false,
+  "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg":false,
+  "https://pbs.twimg.com/profile_images/469217937862037504/yd3-lohT.jpeg":false
 
 }
 
@@ -135,9 +160,9 @@ function filterImage(img){
 function imageRatio(image) {
   var proportion = image.height/image.width;
 
-  if(proportion > 0.9) {
+  if(proportion > 1.1) {
     return "vertical";
-  }else if(proportion < 1.1) {
+  }else if(proportion < 0.9) {
     return "horizontal";
   }else{
     return "square";
