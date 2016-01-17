455ae5085ee116446b3a74db5e19d20a419fcff4
Jake Si <jake.si@live.com>
2016-01-16 22:59:05 -0500

    made constant checks and cache

--- nofap.js
+++ nofap.js
@@ -39,9 +39,9 @@ var myCat = [
   new Cat("square", "http://www.helpinghomelesscats.com/images/cat1.jpg")
 ]
 
-var visitedImages = {}
-
-var uploadContentHash = {};
+var visitedImages = {
+  "http://www.helpinghomelesscats.com/images/cat1.jpg" : false
+}
 
 var nsfwTags = {
   "sexy":true,
@@ -54,19 +54,19 @@ var nsfwTags = {
   "brassiere":true,
   "bra":true,
   "butt":true,
+  "cleavage":true,
+  "penis":true,
+  "dick":true,
+  "naked":true,
   "underwear":true,
   "lingerie":true,
-  "pantie":true,
-  "ocean":true,
-  "symbol":true
+  "pantie":true
 }
 
 function isNSFW(tags){
   var numTags = Math.min(tags.length,10);
   for (var i = 0; i < numTags; i++) {
-    // console.log("tag:" + tags[i].tag);
     if (tags[i] in nsfwTags) {
-      // console.log("NSFW");
       return true;
     };
   };
@@ -88,11 +88,12 @@ function filterImage(img){
       success: function(data){
       console.log(data);       
         if(isNSFW(data.results[0].result.tag.classes)){
-          console.log("NSFW" + img.src);
-           visitedImages[img.imageurl] = true;
+          console.log("NSFW");
+          console.log(img.src);
+           visitedImages[img.src] = true;
           img.src = getCat["horizontal"]()[0].imageurl;
         } else {
-           visitedImages[img.imageurl] = false;
+           visitedImages[img.src] = false;
         }
       },
       error: function(data){
@@ -117,50 +118,50 @@ function imageRatio(image) {
 function fetchApiToken(image){
   $.ajax({ 
 
-      type: "POST",
-      dataType: "json",
-      url: "https://api.clarifai.com/v1/token/",
-      data: {
-        "client_id": "jh5NmLb9r7d0HnmsnrJ7JU9m1kUTasdwf85PHjQw",
-        "client_secret" : "DHEAVtaAHniJACaIpnWBTo0JCyAyhtcFdaoeb-vH",
-        "grant_type" : "client_credentials"
-      },
-      async: false,
-      success: function(data){
-        console.log(data);
-        token = data.access_token;       
-      },
-      error: function(data){
-        console.log(data);
-      }
+    type: "POST",
+    dataType: "json",
+    url: "https://api.clarifai.com/v1/token/",
+    data: {
+      "client_id" : "X__z9Razsn8fGJod6dN3owbKNZpHMuY6W-veigst",
+      "client_secret"  :"GVWwhSi7yvmY7nqidSCMrepgAbtV2_b_zh2QT1tV",
+      "grant_type" : "client_credentials"
+    },
+    async: false,
+    success: function(data){
+      console.log(data);
+      token = data.access_token;       
+    },
+    error: function(data){
+      console.log(data);
+    }
 
   });
 }
 
-function filterImages(){
-     // is cached as nsfw or not in cache ye
-}
-
 (function (document) {
+
+  test = function(){
+    var images = document.getElementsByTagName('img')
+    var length = images.length
+
+    for (var i = 0; i < length; i++) {
+      var ratio = imageRatio(images[i]);
+
+      // is cached as nsfw or not in cache yet
+      if(!(images[i].src in visitedImages)) {
+        if (images[i].height > 75 && images[i].width > 75) {
+          filterImage(images[i]);
+        };
+      }else if(visitedImages[images[i].src]){
+        image[i].src = getCat["horizontal"]()[0].imageurl;
+      }
+      // var number = Randomize(getCat[ratio]());
+    }
+  }
 
   getCat.init(myCat);
 
   fetchApiToken();
-
-  var images = document.getElementsByTagName('img')
-  var length = images.length
-
-  for (var i = 0; i < length; i++) {
-    var uploadContentHash = [];
-    var ratio = imageRatio(images[i]);
-
-    // is cached as nsfw or not in cache yet
-    if(!(images[i].imageurl in visitedImages) || (visitedImages[images[i]].imageurl)) {
-      if (images[i].height > 100 && images[i].width > 100) {
-        filterImage(images[i]);
-      };
-    }
-    // var number = Randomize(getCat[ratio]());
-  }
-
+  test();
+  setInterval(test,1000);
 })(document);
